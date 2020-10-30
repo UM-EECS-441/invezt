605ef4ced81e91981ee1a8c49b4a975280c168f9
Jumanah Colvin <jumanah.colvin@gmail.com>
2020-10-30 14:44:00 -0400

    Add drop down
    Add drop down menu to scan view

--- Invezt/app/src/main/java/edu/umich/invezt/invezt/ScanActivity.kt
+++ Invezt/app/src/main/java/edu/umich/invezt/invezt/ScanActivity.kt
@@ -1,11 +1,30 @@
 package edu.umich.invezt.invezt
 
+import edu.umich.invezt.invezt.R
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.AdapterView.OnItemSelectedListener
 
 class ScanActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_scan)
+
+        val spinner: Spinner = findViewById(R.id.patterns_spinner)
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.patterns_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            spinner.adapter = adapter
+        }
+        spinner.prompt = "Title";
     }
 }
