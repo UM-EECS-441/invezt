769d81fb504efe6e16725b2272bb70bc519d0d04
alhulljar <purpleleeche@gmail.com>
2020-10-29 20:17:16 -0400

    Collapse Expand Menu Implemented
    Collapse/Expand Menu for the Learn View.

---
+++ Invezt/app/src/main/java/edu/umich/invezt/invezt/ExpandableListData.kt
@@ -0,0 +1,41 @@
+            package edu.umich.invezt.invezt
+
+import java.util.*
+internal object ExpandableListData {
+    val data: HashMap<String, List<String>>
+        get() {
+            val expandableListDetail =
+                HashMap<String, List<String>>()
+            val supportLine: MutableList<String> = ArrayList()
+            supportLine.add("Summary")
+            supportLine.add("Links")
+            val resistanceLine: MutableList<String> = ArrayList()
+            resistanceLine.add("Summary")
+            resistanceLine.add("Links")
+            val bearFlag: MutableList<String> = ArrayList()
+            bearFlag.add("Summary")
+            bearFlag.add("Links")
+            val bullFlag: MutableList<String> = ArrayList()
+            bullFlag.add("Summary")
+            bullFlag.add("Links")
+            val cupFlag: MutableList<String> = ArrayList()
+            cupFlag.add("Summary")
+            cupFlag.add("Links")
+            val channelFlag: MutableList<String> = ArrayList()
+            channelFlag.add("Summary")
+            channelFlag.add("Links")
+            val elliotFlag: MutableList<String> = ArrayList()
+            elliotFlag.add("Summary")
+            elliotFlag.add("Links")
+
+            expandableListDetail["SUPPORT LINES"] = supportLine
+            expandableListDetail["RESISTANCE LINES"] = resistanceLine
+            expandableListDetail["BEAR FLAG"] = bearFlag
+            expandableListDetail["BULL FLAG"] = bullFlag
+            expandableListDetail["CUP & HANDLE"] = cupFlag
+            expandableListDetail["CHANNEL"] = channelFlag
+            expandableListDetail["ELLIOT WAVES"] = elliotFlag
+
+            return expandableListDetail
+        }
+}
--- Invezt/app/src/main/java/edu/umich/invezt/invezt/LearnActivity.kt
+++ Invezt/app/src/main/java/edu/umich/invezt/invezt/LearnActivity.kt
@@ -1,11 +1,28 @@
 package edu.umich.invezt.invezt
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.ExpandableListAdapter
+import android.widget.ExpandableListView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import edu.umich.invezt.invezt.ExpandableListData.data
 
 class LearnActivity : AppCompatActivity() {
+    private var expandableListView: ExpandableListView? = null
+    private var adapter: ExpandableListAdapter? = null
+    private var titleList: List<String>? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_learn)
+        title = getString(R.string.app_name)
+        expandableListView = findViewById(R.id.expendableList)
+        if (expandableListView != null) {
+            val listData = data
+            titleList = ArrayList(listData.keys)
+            adapter = CustomExpandableListAdapter(this, titleList as ArrayList<String>, listData)
+            expandableListView!!.setAdapter(adapter)
+        }
     }
 }
